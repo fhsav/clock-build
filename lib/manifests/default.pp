c480456777718d8790dd88f7f0a12e47d159909e
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-06-18 22:24:56 -0400

    work on manifest.

--- lib/manifests/default.pp
+++ lib/manifests/default.pp
@@ -5,6 +5,7 @@ Exec {
   path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin']
 }
 
+
 ###
 # Pre-install Stage
 ###
@@ -21,4 +22,43 @@ class apt_get_update {
 
 class { 'apt_get_update':
   stage => preinstall
+}
+
+
+###
+# Preliminary dependencies
+##
+
+package { ['build-essential', 'make', 'bison', 'openssl', 'libreadline5', 'curl', 'libcurl4-gnutls-dev', 'libssl-dev', 'libxml2-dev', 'libreadline6-dev', 'libyaml-dev', 'zlib1g-dev', 'libsasl2-dev']:
+  ensure => installed;
+}
+
+
+###
+# Databases 'n such
+###
+
+package { ['mongodb', 'redis-server', 'memcached', 'nodejs']:
+  ensure => installed;
+}
+
+
+###
+# Ruby
+###
+
+rbenv::install { 'ubuntu':
+  group => 'ubuntu',
+  home  => '/home/ubuntu'
+}
+
+rbenv::plugin { 'rbenv-sudo':
+  user   => 'ubuntu',
+  source => 'https://github.com/dcarley/rbenv-sudo.git'
+}
+
+rbenv::compile { '2.0.0-p0':
+  user   => 'ubuntu',
+  home   => '/home/ubuntu',
+  global => true
 }
